73e83553089a26a7681a98de21e66c56fc67aa36
Twitcinhhh <vinhlt@sonat.vn>
2026-09-05 08:59:19 +0700

    asset+ script decompline

---
+++ Assets/Scripts/StardewValley/FriendshipStatus.cs
@@ -0,0 +1,19 @@
+#if false
+// Decompiled with JetBrains decompiler
+// Type: StardewValley.FriendshipStatus
+// Assembly: Stardew Valley, Version=1.5.6.22018, Culture=neutral, PublicKeyToken=null
+// MVID: BEBB6D18-4941-4529-AC12-B54F0C61CC20
+// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Stardew Valley.dll
+
+namespace StardewValley
+{
+  public enum FriendshipStatus
+  {
+    Friendly,
+    Dating,
+    Engaged,
+    Married,
+    Divorced,
+  }
+}
+#endif
---
+++ Assets/Scripts/StardewValley/SDKs/LobbyUpdateListener.cs
@@ -0,0 +1,15 @@
+#if false
+// Decompiled with JetBrains decompiler
+// Type: StardewValley.SDKs.LobbyUpdateListener
+// Assembly: Stardew Valley, Version=1.5.6.22018, Culture=neutral, PublicKeyToken=null
+// MVID: BEBB6D18-4941-4529-AC12-B54F0C61CC20
+// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Stardew Valley.dll
+
+namespace StardewValley.SDKs
+{
+  public interface LobbyUpdateListener
+  {
+    void OnLobbyUpdate(object lobby);
+  }
+}
+#endif
